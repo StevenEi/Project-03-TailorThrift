e0a57429d39ebafd7d3404843cbf4d8e95bfd2a0
Marc Pacampara <mpacampara@gmail.com>
2022-02-23 11:03:16 -0800

    added productcards.js, moved public folder into client folder as a subfolder

---
+++ client/src/components/ProductCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+
+export default function ProductCards(){
+  return (
+    <div>ProductCards</div>
+  )
+}
+
+export default ProductCards
