b457af591834bc8414d60432ec1efd726c201f2a
Matthew Walker <Matthewwalkermw64@gmail.com>
2022-02-24 17:23:44 -0800

    created cart JS. file and importing utils files to help establish cart functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Cart from "../components/Cart";
+
 
 const Home = () => {
   return (
     <div>
         Home page
+         {/* currently using boiler plate element from activities */}
+        <Cart / >
     </div>
   )
 }
