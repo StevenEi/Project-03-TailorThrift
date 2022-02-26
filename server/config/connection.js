a5a3661d10c801b07145a935cb8865e2298696d8
Oscar Plumridge <Bigop76.op@gmail.com>
2022-02-25 16:54:10 -0800

    small backend changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/taylorthrift',
+  process.env.MONGODB_URI || 'mongodb://localhost/tailorthrift',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
