bb0d499513f396c88f23c2b732cff76168633fcb
Steven Eiselt <seiselts@gmail.com>
2022-02-28 09:49:42 -0800

    updated route syntax

--- client/src/App.js
+++ client/src/App.js
@@ -72,9 +72,9 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/products" element={<Products />} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" element={<Success />} />
+              <Route exact path="/orderHistory" element={<OrderHistory />} />
+              <Route exact path="/products/:id" element={<Detail />} />
             </Routes>
             <Footer />
           </BrowserRouter>
