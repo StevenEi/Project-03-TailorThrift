c7a4e47a209a83871b18cf830aa49a5e4c8e2e25
Marc Pacampara <mpacampara@gmail.com>
2022-03-04 12:44:21 -0800

    finished styling signup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,7 +3,7 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
-import '../assets/css/Login.css'
+import '../assets/css/LoginSignUp.css'
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import '../assets/css/LoginSignUp.css'
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -36,94 +37,119 @@ function Signup(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
 
-      <h2>Signup</h2>
+      <div class='loginCard'>
+        <h2 class='loginTitle'>New Users</h2>
+      </div>
+
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+        <div className="flex-row space-between my-2 fields">
+          <label htmlFor="firstName"></label>
           <input
-            placeholder="First"
+            className='loginField'
+            placeholder="First Name"
             name="firstName"
             type="firstName"
             id="firstName"
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
+        <div className="flex-row space-between my-2 fields">
+          <label htmlFor="lastName"></label>
           <input
-            placeholder="Last"
+            className='loginField'
+            placeholder="Last Name"
             name="lastName"
             type="lastName"
             id="lastName"
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
+        <div className="flex-row space-between my-2 fields">
+          <label htmlFor="email"></label>
           <input
-            placeholder="youremail@test.com"
+            className='loginField'
+            placeholder="email@domain.com"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+        <div className="flex-row space-between my-2 fields">
+          <label htmlFor="pwd"></label>
           <input
+            className='loginField'
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
           />
+                    <div >
+            <p class='measureTitle'>New User Measurements</p>
+          </div>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="chest">Chest:</label>
-          <input
-            placeholder="e.g. 42in"
-            name="chest"
-            type="chest"
-            id="chest"
-            onChange={handleChange}
-          />
+
+        <div class='measureCard'>
+
+
+          <div className="measureFields">
+            <label class='measureTitleText' htmlFor="chest">Chest </label>
+            <input
+              className='measureField'
+              placeholder="  e.g.42in"
+              name="chest"
+              type="chest"
+              id="chest"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="measureFields">
+            <label class='measureTitleText' htmlFor="pwd">Arms</label>
+            <input
+              className='measureField'
+              placeholder="e.g. 42in"
+              name="arms"
+              type="arms"
+              id="arms"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="measureFields">
+            <label class='measureTitleText' htmlFor="waist">Waist</label>
+            <input
+              className='measureField'
+              placeholder="e.g. 32in"
+              name="waist"
+              type="waist"
+              id="waist"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="measureFields">
+            <label class='measureTitleText' htmlFor="legs">Inseam</label>
+            <input
+              className='measureField'
+              placeholder="e.g. 32in"
+              name="inseam"
+              type="inseam"
+              id="inseam"
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Arms:</label>
-          <input
-            placeholder="e.g. 42in"
-            name="arms"
-            type="arms"
-            id="arms"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="waist">Waist:</label>
-          <input
-            placeholder="e.g. 32in"
-            name="waist"
-            type="waist"
-            id="waist"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="legs">Inseam:</label>
-          <input
-            placeholder="e.g. 32in"
-            name="inseam"
-            type="inseam"
-            id="inseam"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+
+        <div className="flex-end loginButton">
+          <button type="submit" class='loginBtn'>Sign Up</button>
         </div>
       </form>
+      <div>
+      <div class='signUpRedirect'>Already a member?</div>
+      <Link to="/login" class='goToLogin'>← Go to Login</Link>
+      </div>
+
+
     </div>
   );
 }
