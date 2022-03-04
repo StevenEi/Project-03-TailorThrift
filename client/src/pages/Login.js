9290267bd4817d356d7ec253b41f8482399002a5
Jeff Wymore <yardboy2401@gmail.com>
2022-03-04 15:53:05 -0800

    fixed login popup for correct logout button based on loggedIn logic

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
-import '../assets/css/LoginSignUp.css'
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+import "../assets/css/LoginSignUp.css";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
@@ -32,48 +32,75 @@ function Login(props) {
 
   return (
     <div className="container my-1">
+      <div className="loginCard">
+        <h2 class="loginTitle">Registered customers</h2>
 
-      <div className='loginCard'>
-        <h2 class='loginTitle'>Registered customers</h2>
-
-        <form onSubmit={handleFormSubmit}>
-          <div className="flex-row space-between my-2 fields">
-            <label htmlFor="email"></label>
-            <input
-              className='loginField topfield'
-              placeholder="Email"
-              name="email"
-              type="email"
-              id="email"
-              onChange={handleChange}
-            />
+        {Auth.loggedIn() ? (
+          <div className="flex-row flex-end logoutButton">
+            <button
+              type="submit"
+              class="logoutBtn"
+              onClick={() => Auth.logout()}
+            >
+              Logout
+            </button>
           </div>
-          <div className="flex-row space-between my-2 fields">
-            <label htmlFor="pwd"></label>
-            <input
-              className='loginField'
-              placeholder="Password"
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={handleChange}
-            />
-          </div>
-          {error ? (
-            <div>
-              <p className="error-text">The provided credentials are incorrect</p>
+        ) : (
+          <form onSubmit={handleFormSubmit}>
+            <div className="flex-row space-between my-2 fields">
+              <label htmlFor="email"></label>
+              <input
+                className="loginField topfield"
+                placeholder="Email"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
-          ) : null}
-          {Auth.loggedIn() ? (<div className="flex-row flex-end logoutButton">
-            <button type="submit" class='logoutBtn' onClick={() => Auth.logout()}>Logout</button>
-          </div>) : (<div className="flex-row flex-end loginButton">
-            <button type="submit" class='loginBtn'>Login</button>
-          </div>)}
-        </form>
+            <div className="flex-row space-between my-2 fields">
+              <label htmlFor="pwd"></label>
+              <input
+                className="loginField"
+                placeholder="Password"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            {error ? (
+              <div>
+                <p className="error-text">
+                  The provided credentials are incorrect
+                </p>
+              </div>
+            ) : null}
+            {Auth.loggedIn() ? (
+              <div className="flex-row flex-end logoutButton">
+                <button
+                  type="submit"
+                  class="logoutBtn"
+                  onClick={() => Auth.logout()}
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div className="flex-row flex-end loginButton">
+                <button type="submit" class="loginBtn">
+                  Login
+                </button>
+              </div>
+            )}
+          </form>
+        )}
       </div>
-      <div className='redirectSection'>
-        <div class='loginRedirect'>Not a member?</div>
-        <Link to="/signup" class='goToSignUp'><span>←</span> Go To Signup</Link>
+      <div className="redirectSection">
+        <div class="loginRedirect">Not a member?</div>
+        <Link to="/signup" class="goToSignUp">
+          <span>←</span> Go To Signup
+        </Link>
       </div>
     </div>
   );
